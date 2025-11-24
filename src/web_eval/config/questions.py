1c32204fc6aeaef18d200eb1c9421d37f6a5b370
CommonGibbon <xenoproboscizoid@gmail.com>
2025-11-23 20:45:26 -0800

    updated feedback questions to include an /10 score for appropriate questions

--- src/web_eval/config/questions.py
+++ src/web_eval/config/questions.py
@@ -3,7 +3,7 @@ from typing import List
 
 # Sample evaluation questions
 feedback_questions: List[str] = [
-    "How satisfied were you with thhe overall website experience?",
-    "How likely are you to purchase products from this website?",
+    "How satisfied were you with the overall website experience? Rate out of 10.",
+    "How likely are you to purchase products from this website? Rate out of 10.",
     "Were there any pain points in your shopping exerience? If so, what were they?"
 ]
